5c3cc0bcb0ed221b6bcc2ad1489f1d2d8a2e101f
Kotyk Roman Viktorovich <sleeporsmile@gmail.com>
2019-10-28 23:58:28 +0200

    structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React ,{Component} from 'react';
-import history from './utils/history'
-import LayoutDefault from './components/layouts/LayoutDefault'
-import * as routes from './config/routes'
-import Notice from './components/Notice'
-import { Router, Switch,Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import history from './utils/history';
+import LayoutDefault from './components/layouts/LayoutDefault';
+import * as routes from './config/routes';
+import Notice from './components/Notice';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
@@ -13,33 +13,32 @@ import MyAccount from './pages/MyAccount';
 import ProductDetails from './pages/ProductDetails';
 import ProductList from './pages/ProductList';
 import Footer from './components/Footer';
-import Header from './components/Header'
+import Header from './components/Header';
 
- export  default class App extends Component {
-  //
+
+export default class App extends Component {
+
   render() {
     return (
-      <Router history={history}>
-        <>
+        <Router history={history}>
+
           <Header/>
           <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/cart" component={Cart} />
-              <Route exact path="/checkout" component={Checkout} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/account" component={MyAccount} />
-              <Route exact path="/product/:id" component={ProductDetails} />
-              <Route exact path="/products" component={ProductList} />
-              <Route exact path="*" component={NotFound} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/cart" component={Cart} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/account" component={MyAccount} />
+            <Route exact path="/product/:id" component={ProductDetails} />
+            <Route exact path="/products" component={ProductList} />
+            <Route exact path="*" component={NotFound} />
             <LayoutDefault exact path={routes.HOME} component={Home} />
 
             <LayoutDefault component={NotFound} />
           </Switch>
-          <Footer/>
+          <Footer />
           <Notice />
-        </>
       </Router>
     );
   }
 }
-
