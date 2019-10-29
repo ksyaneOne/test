29c70cde058b90e4e9a195490f6d4d33e8b11716
Kotyk Roman Viktorovich <sleeporsmile@gmail.com>
2019-10-29 21:05:46 +0200

    add travis

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 
  export  default class App extends Component {
+
   render() {
     return (
       <Router history={history}>
