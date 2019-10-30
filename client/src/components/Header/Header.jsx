a1d9650a8220c8805832aa062fcb06bab67cc5d3
Konstantin <konst2302@gmail.com>
2019-10-30 19:51:54 +0200

    HeaderStart

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <div className="container"></div>
+        <div className="container">Header</div>
       </header>
     );
   }
