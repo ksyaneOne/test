8ebbc04dd7ca267a73ebd02b680f01e38cee8347
Konstantin <konst2302@gmail.com>
2019-10-30 23:19:02 +0200

    Header#1

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,41 @@
-import React ,{Component} from 'react';
-import history from './utils/history'
-import LayoutDefault from './components/layouts/LayoutDefault'
-import * as routes from './config/routes'
-import Notice from './components/Notice/Notice'
-import { Router, Switch,Route } from "react-router-dom";
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
-import MyAccount from './pages/MyAccount';
-import ProductDetails from './pages/ProductDetails';
-import ProductList from './pages/ProductList';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import React, { Component } from "react";
+import history from "./utils/history";
+import LayoutDefault from "./components/layouts/LayoutDefault";
+import * as routes from "./config/routes";
+import Notice from "./components/Notice/Notice";
+import { Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import MyAccount from "./pages/MyAccount";
+import ProductDetails from "./pages/ProductDetails";
+import ProductList from "./pages/ProductList";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "semantic-ui-css/semantic.min.css";
 
- export  default class App extends Component {
-
+export default class App extends Component {
   render() {
     return (
       <Router history={history}>
         <>
-          <Header/>
+          <Header />
           <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/cart" component={Cart} />
-              <Route exact path="/checkout" component={Checkout} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/account" component={MyAccount} />
-              <Route exact path="/product/:id" component={ProductDetails} />
-              <Route exact path="/products" component={ProductList} />
-              <Route exact path="*" component={NotFound} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/cart" component={Cart} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/account" component={MyAccount} />
+            <Route exact path="/product/:id" component={ProductDetails} />
+            <Route exact path="/products" component={ProductList} />
+            <Route exact path="*" component={NotFound} />
             <LayoutDefault exact path={routes.HOME} component={Home} />
 
             <LayoutDefault component={NotFound} />
           </Switch>
-          <Footer/>
+          <Footer />
           <Notice />
         </>
       </Router>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,61 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "semantic-ui-react";
+import styled from "@emotion/styled";
+import "reset-css";
 
+const HeaderSearchInput = styled.input`
+  display: none;
+`;
 class Header extends Component {
+  state = {
+    onMouse: false
+  };
+  openSearchInput = () => {
+    document.getElementById("HeaderSearchInput").style.display = "inline-block";
+  };
+
   render() {
     return (
-      <header>
-        <div className="container">Header</div>
-      </header>
+      <div>
+        <div>
+          <Link to="/">Logotype</Link>
+          <HeaderSearchInput
+            type="text"
+            id="HeaderSearchInput"
+            display="none"
+          />
+          <Icon name="search" onMouseEnter={this.openSearchInput} /> Search
+          <Link to="/account">
+            <Icon name="user" /> My account
+          </Link>
+          <Link to="/cart">
+            <Icon name="shopping cart" /> My cart
+          </Link>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link to="/products">Moto Helmets</Link>
+            </li>
+            <li>
+              <Link to="/products">Moto Jackets</Link>
+            </li>
+            <li>
+              <Link to="/products">Moto Pants</Link>
+            </li>
+            <li>
+              <Link to="/products">Moto Gloves</Link>
+            </li>
+            <li>
+              <Link to="/products">Moto boots</Link>
+            </li>
+            <li>
+              <Link to="/products">Accessories</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
