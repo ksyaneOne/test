84e6a341ef97092ff81a0606d2b6bcc3a90391e2
Sopruk <alexandr.programer@gmail.com>
2019-11-03 10:02:48 +0100

    added component react-multi-carousel

---
+++ client/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+const MainCarousel = (props) => {
+	return (
+		<Carousel {...props.carouselSettings} >
+			{props.elements}
+		</Carousel>
+	)
+}
+
+export default MainCarousel;
