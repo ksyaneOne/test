9cce5eda17c7b864896ee2baaad1793e5c48ef7a
Ivanchenkois <56307773+Ivanchenkois@users.noreply.github.com>
2019-11-06 02:00:23 +0200

    ScrollButton (#29)

---
+++ client/src/components/ScrollButton/ScrollButton.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import ScrollButton from 'react-scroll-button';
+
+class ScrollToTop extends Component {
+
+    render() {
+        return (
+            <ScrollButton 
+				targetId={'root'}
+                behavior={'smooth'} 
+                buttonBackgroundColor={'black'}
+                iconType={'angle-up'}
+                scrollSpeed={'2s'}
+            />
+        );
+    }
+};
+
+export default ScrollToTop;
