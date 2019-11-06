ab9857d8c7fa5283cef86bf0af5394475d1bead2
Kotyk Roman Viktorovich <sleeporsmile@gmail.com>
2019-11-06 22:03:04 +0200

    Merge branch 'develop' of /Users/kotseller/Documents/PROGA/DAN-IT/My Projects/Online_Shop with conflicts.

--- client/src/services/api/initialConfig.js
+++ client/src/services/api/initialConfig.js
@@ -1,20 +1,18 @@
 import { success as noticeSuccess, error as noticeError } from 'react-notification-system-redux';
-import store from 'store';
-import logger from "utils/logger";
-import history from 'utils/history';
-import * as ROUTES from 'config/routes';
+import logger from 'utils/logger';
+import * as store from '../../store';
+import history from '../../utils/history';
+import * as ROUTES from '../../config/routes';
 
 
-var initialConfig = {
+const initialConfig = {
   timeout: 10000,
-  validateStatus: status => {
-    return status;
-  }
+  validateStatus: (status) => status,
 };
 
 function handleInterceptorsResponse(response) {
   if (response.status >= 400) {
-    logger(response)
+    logger(response);
   }
 
   if (response.status === 401) {
@@ -23,21 +21,19 @@ function handleInterceptorsResponse(response) {
 
   if (response.status === 430) {
     // logout
+
   }
 
   return response;
-};
-
-const handleInterceptorsError = error => {
-  // store.dispatch(noticeError({
-  // 	message: error.message
-  // }));
-
-  return { error, data: {} };
 }
 
+const handleInterceptorsError = (error) => {
+  store.store.dispatch(noticeError({ message: error.message }));
+
+  ({ error, data: {} });
+};
 export {
   initialConfig as default,
   handleInterceptorsResponse,
-  handleInterceptorsError
-}
+  handleInterceptorsError,
+};
