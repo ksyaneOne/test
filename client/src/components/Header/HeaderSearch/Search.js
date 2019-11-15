f18e5faec467bfb6720c7a755c5447bdd67cea1d
Konstantin <konst2302@gmail.com>
2019-11-15 21:18:47 +0200

    header-last

--- client/src/components/Header/HeaderSearch/Search.js
+++ client/src/components/Header/HeaderSearch/Search.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Icon, Input } from 'semantic-ui-react';
-
+import React, { useState, useEffect } from 'react';
+import { Input, Segment, Dimmer, Loader } from 'semantic-ui-react';
 import HeaderSearchStyle from './style';
 
 function HeaderSearch(props) {
+  const [query, setQuery] = useState('');
+  const handleInputQuery = e => setQuery(e.target.value);
+
   return (
     <HeaderSearchStyle>
       <Input
+        value={query}
+        onChange={handleInputQuery}
         inverted
         fluid
         icon={{ name: 'search', circular: true, link: true }}
