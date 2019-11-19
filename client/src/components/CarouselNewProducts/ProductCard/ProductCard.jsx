cfe7e1b19be67856c44a92a57690b3dee35e2914
Sopruk <alexandr.programer@gmail.com>
2019-11-19 22:33:40 +0100

    added CardWrapper

--- client/src/components/CarouselNewProducts/ProductCard/ProductCard.jsx
+++ client/src/components/CarouselNewProducts/ProductCard/ProductCard.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { Card, Button, Label } from 'semantic-ui-react';
+import { Card, Button, Label, Segment } from 'semantic-ui-react';
 import 'react-multi-carousel/lib/styles.css';
-import ImgWrapper from './style';
+import ImgWrapper from './imgWrapper';
+import CardWrapper from './cardWrapper';
 
 function CardExampleCard(props) {
   const { product } = props;
   return (
-    <Card>
-      <ImgWrapper>
-        <img alt={product.name} src={product.imageUrls[0]} />
-        <Label color="red" ribbon="right">
-          New
-        </Label>
-      </ImgWrapper>
-      <Card.Content>
-        <Card.Header>{product.name}</Card.Header>
-        <div>{product.previousPrice}$</div>
-      </Card.Content>
-      <Card.Content extra>
-        <div className="ui two buttons">
-          <Button basic color="black">
-            Add to Card
-          </Button>
-          <Button secondary>Buy</Button>
-        </div>
-      </Card.Content>
-    </Card>
+    <CardWrapper>
+      <Segment raised>
+        <Card>
+          <ImgWrapper>
+            <img alt={product.name} src={product.imageUrls[0]} />
+            <Label color="red" ribbon="right">
+              New
+            </Label>
+          </ImgWrapper>
+          <Card.Content>
+            <Card.Header>{product.name}</Card.Header>
+            <div>{product.previousPrice}$</div>
+          </Card.Content>
+          <Card.Content extra>
+            <div className="ui two buttons">
+              <Button basic color="black">
+                Add to Card
+              </Button>
+              <Button secondary>Buy</Button>
+            </div>
+          </Card.Content>
+        </Card>
+      </Segment>
+    </CardWrapper>
   );
 }
 
--- client/src/components/Catalog/CategoryCard/CategoryCard.jsx
+++ client/src/components/Catalog/CategoryCard/CategoryCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
 import 'react-multi-carousel/lib/styles.css';
-import ImgWrapper from './style';
+import ImgWrapper from './imgWrapper';
+import CardWrapper from './cardWrapper';
 
 function CategoryCard(props) {
   const { category } = props;
   return (
-    <Card>
-      <ImgWrapper>
-        <img alt={category.name} src={category.imgUrl} />
-      </ImgWrapper>
-      <Card.Content>
-        <Card.Header>{category.name}</Card.Header>
-      </Card.Content>
-    </Card>
+    <CardWrapper>
+      <Card>
+        <ImgWrapper>
+          <img alt={category.name} src={category.imgUrl} />
+        </ImgWrapper>
+        <Card.Content>
+          <Card.Header>{category.name}</Card.Header>
+        </Card.Content>
+      </Card>
+    </CardWrapper>
   );
 }
 export default CategoryCard;
