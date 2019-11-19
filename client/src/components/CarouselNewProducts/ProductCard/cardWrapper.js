ac925a6cce884fc92c382b5043b0b524ac6cbc1d
Sopruk <alexandr.programer@gmail.com>
2019-11-19 22:33:02 +0100

    styled cards

---
+++ client/src/components/CarouselNewProducts/ProductCard/cardWrapper.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const CardWrapper = styled.div`
+  margin: 10px;
+  &:hover {
+    margin: 5px;
+    box-shadow: 0px 0px 5px 5px rgba(0, 0, 0, 0.75);
+  }
+`;
+
+export default CardWrapper;
