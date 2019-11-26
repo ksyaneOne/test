cd9d586eeaa0a56cf4d1a5dbeb84984c30de6131
Sopruk <alexandr.programer@gmail.com>
2019-11-26 16:56:01 +0100

    added actions category and Product

---
+++ client/src/actions/category.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+// Fetch all CATEGORIES
+export const FETCH_CATEGORIES_START = 'FETCH_CATEGORIES_START';
+export const FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS';
+export const FETCH_CATEGORIES_FAILURE = 'FETCH_CATEGORIES_FAILURE';
+
+const fetchCategoriesApi = async () => {
+  return axios
+    .get('/catalog')
+    .then(catalog => {
+      return catalog.data;
+    })
+    .catch(err => {
+      return err;
+    });
+};
+
+const getCategory = () => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: FETCH_CATEGORIES_START,
+        payload: { loading: false }
+      });
+      const categories = await fetchCategoriesApi();
+      dispatch({
+        type: FETCH_CATEGORIES_SUCCESS,
+        payload: { categories, loading: true }
+      });
+    } catch (err) {
+      dispatch({
+        type: FETCH_CATEGORIES_FAILURE,
+        payload: err,
+        error: true
+      });
+    }
+  };
+};
+export default getCategory;
---
+++ client/src/actions/products.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+// Fetch all PRODUCTS
+export const FETCH_PRODUCTS_START = 'FETCH_PRODUCTS_START';
+export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
+
+const fetchProductsApi = async () => {
+  return axios
+    .get('/products')
+    .then(product => {
+      return product.data;
+    })
+    .catch(err => {
+      return err;
+    });
+};
+
+const getProducts = () => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: FETCH_PRODUCTS_START,
+        payload: { loading: false }
+      });
+      const products = await fetchProductsApi();
+      dispatch({
+        type: FETCH_PRODUCTS_SUCCESS,
+        payload: { products, loading: true }
+      });
+    } catch (err) {
+      dispatch({
+        type: FETCH_PRODUCTS_FAILURE,
+        payload: { err, loading: false },
+        error: true
+      });
+    }
+  };
+};
+
+export default getProducts;
