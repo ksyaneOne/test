7871de54121f6adca94a1db7ebeaedd49794e7cf
Sopruk <alexandr.programer@gmail.com>
2019-11-26 17:01:10 +0100

    moved  Link to card

--- client/src/components/CarouselNewProducts/ProductCard/ProductCard.jsx
+++ client/src/components/CarouselNewProducts/ProductCard/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Button, Label, Segment, Icon } from 'semantic-ui-react';
 import 'react-multi-carousel/lib/styles.css';
 import ImgWrapper from './imgWrapper';
@@ -10,16 +11,18 @@ const NewProduct = props => {
     <CardWrapper>
       <Segment raised>
         <Card>
-          <ImgWrapper>
-            <img alt={product.name} src={product.imageUrls[0]} />
-            <Label color="red" ribbon="right">
-              New
-            </Label>
-          </ImgWrapper>
-          <Card.Content>
-            <Card.Header>{product.name}</Card.Header>
-            <div>{product.previousPrice}$</div>
-          </Card.Content>
+          <Link to={{ pathname: `/product/${product.itemNo}`, product: product }}>
+            <ImgWrapper>
+              <img alt={product.name} src={product.imageUrls[0]} />
+              <Label color="red" ribbon="right">
+                New
+              </Label>
+            </ImgWrapper>
+            <Card.Content>
+              <Card.Header>{product.name}</Card.Header>
+              <div>{product.previousPrice}$</div>
+            </Card.Content>
+          </Link>
           <Card.Content>
             <div>
               <Button>
