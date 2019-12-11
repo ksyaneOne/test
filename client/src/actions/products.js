dddbd6be9cb8cb690d4328d22d2e3ae5dde1f02d
Sopruk <alexandr.programer@gmail.com>
2019-12-11 21:59:33 +0100

    moved fetchProduct to API

--- client/src/actions/products.js
+++ client/src/actions/products.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import API from '../services/api/api';
 
 // Fetch all PRODUCTS
 export const FETCH_PRODUCTS_START = 'FETCH_PRODUCTS_START';
@@ -10,27 +10,15 @@ export const FETCH_PRODUCTBYID_START = 'FETCH_PRODUCT_BY_ID_START';
 export const FETCH_PRODUCTBYID_SUCCESS = 'FETCH_PRODUCT_BY_ID_SUCCESS';
 export const FETCH_PRODUCTBYID_FAILURE = 'FETCH_PRODUCT_BY_ID_FAILURE';
 
-const fetchProductsApi = async () => {
-  return axios
-    .get('/products')
-    .then(products => {
-      return products.data;
-    })
-    .catch(err => {
-      return err;
-    });
-};
+// Fetch  PRODUCTS By Filter Query
+export const FETCH_PRODUCTS_BY_FILTER_QUERY_START = 'FETCH_PRODUCTS_BY_FILTER_QUERY_START';
+export const FETCH_PRODUCTS_BY_FILTER_QUERY_SUCCESS = 'FETCH_PRODUCTS_BY_FILTER_QUERY_SUCCESS';
+export const FETCH_PRODUCTS_BY_FILTER_QUERY_FAILURE = 'FETCH_PRODUCTS_BY_FILTER_QUERY_FAILURE';
 
-const fetchProductByIdApi = async id => {
-  return axios
-    .get(`/products/${id}/`)
-    .then(products => {
-      return products.data;
-    })
-    .catch(err => {
-      return err;
-    });
-};
+// Fetch  PRODUCTS By Search Phrases
+export const FETCH_PRODUCTS_BY_SEARCH_PHRASE_START = 'FETCH_PRODUCTS_BY_SEARCH_PHRASE_START';
+export const FETCH_PRODUCTS_BY_SEARCH_PHRASE_SUCCESS = 'FETCH_PRODUCTS_BY_SEARCH_PHRASE_SUCCESS';
+export const FETCH_PRODUCTS_BY_SEARCH_PHRASE_FAILURE = 'FETCH_PRODUCTS_BY_SEARCH_PHRASE_FAILURE';
 
 export const getProducts = () => {
   return async dispatch => {
@@ -39,7 +27,7 @@ export const getProducts = () => {
         type: FETCH_PRODUCTS_START,
         payload: { loading: false }
       });
-      const products = await fetchProductsApi();
+      const products = await API.product.fetchProductsAll();
       if (products.name === 'Error') {
         throw new Error(products.message);
       }
@@ -63,7 +51,7 @@ export const getProductById = id => {
         type: FETCH_PRODUCTBYID_START,
         payload: { loading: false }
       });
-      const product = await fetchProductByIdApi(id);
+      const product = await API.product.fetchProductById(id);
       dispatch({
         type: FETCH_PRODUCTBYID_SUCCESS,
         payload: { product, loading: true }
@@ -71,6 +59,54 @@ export const getProductById = id => {
     } catch (err) {
       dispatch({
         type: FETCH_PRODUCTBYID_FAILURE,
+        payload: { error: err, loading: true }
+      });
+    }
+  };
+};
+
+export const getProductsByFilterQuery = query => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: FETCH_PRODUCTS_BY_FILTER_QUERY_START,
+        payload: { loading: false }
+      });
+      const products = await API.product.fetchProductsByFilterQuery(query);
+      if (products.name === 'Error') {
+        throw new Error(products.message);
+      }
+      dispatch({
+        type: FETCH_PRODUCTS_BY_FILTER_QUERY_SUCCESS,
+        payload: { products, loading: true }
+      });
+    } catch (err) {
+      dispatch({
+        type: FETCH_PRODUCTS_BY_FILTER_QUERY_FAILURE,
+        payload: { error: err, loading: true }
+      });
+    }
+  };
+};
+
+export const getProductsBySearchPhrases = phrases => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: FETCH_PRODUCTS_BY_SEARCH_PHRASE_START,
+        payload: { loading: false }
+      });
+      const products = await API.product.fetchProductsBySearchPhrases(phrases);
+      if (products.name === 'Error') {
+        throw new Error(products.message);
+      }
+      dispatch({
+        type: FETCH_PRODUCTS_BY_SEARCH_PHRASE_SUCCESS,
+        payload: { products, loading: true }
+      });
+    } catch (err) {
+      dispatch({
+        type: FETCH_PRODUCTS_BY_SEARCH_PHRASE_FAILURE,
         payload: { error: err, loading: true }
       });
     }
