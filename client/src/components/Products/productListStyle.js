27968d6ab456bcdf2f34f6c2bd2c095c03159e45
Ivanchenko.is <alonedoter@gmail.com>
2019-12-13 00:48:35 +0200

    add product page

--- client/src/components/Products/productListStyle.js
+++ client/src/components/Products/productListStyle.js
@@ -1,18 +1,23 @@
 import styled from '@emotion/styled';
 
-const ImageConteiner = styled.div`
+export const ImageContainer = styled.div`
   position: relative;
-  overflow: hidden;
+  overflow: hiden;
   height: 240px;
   img {
     position: absolute;
     top: 50%;
     left: 50%;
-    width: 200px;
     height: 200px;
     object-fit: cover;
     transform: translate(-50%, -50%);
   }
 `;
 
-export default ImageConteiner;
+export const BlackLine = styled.div`
+  display: inline-block;
+  height: 5px;
+  width: 240px;
+  border-radius:2px;
+  background-color: black;
+`;
---
+++ client/src/pages/ProductList/ProductListStyle.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+	max-width: 1100px;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 30px;
+`;
+export default Container;
