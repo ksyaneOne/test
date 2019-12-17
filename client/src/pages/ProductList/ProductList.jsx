12d36a479f60962287106f6a78b2347afb88c77c
Sopruk <alexandr.programer@gmail.com>
2019-12-17 20:55:24 +0100

    edited ProductList

--- client/src/pages/ProductList/ProductList.jsx
+++ client/src/pages/ProductList/ProductList.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
-import { Dimmer, Loader, Card } from 'semantic-ui-react';
-import ProductCard from '../../components/Products';
-import useFetch from '../../utils/useFetch';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getProductsByFilterQuery } from '../../actions/products';
+import { Segment, Header, Grid } from 'semantic-ui-react';
+import ProductCart from '../../components/CarouselNewProducts/ProductCard';
 
-export default function ProductList() {
-	const { loading, data } = useFetch('/products', [] );
-	const products = data.map(p=>(
-  <div key={p._id}>
-    <ProductCard props={p} />
-  </div>
-		)
-	);
-	if (loading)
-		return (
-  <Dimmer active>
-    <Loader />
-  </Dimmer>
-		);
+const ProductDetails = props => {
+  const { onGetProductsByFilter, match, products = [], loading } = props;
+  const { params } = match;
+  const { id } = params;
+
+  console.log(props);
+
+  useEffect(() => {
+    onGetProductsByFilter(`categories=${id}`);
+  }, [id]);
+  console.log(props, 'props');
+
+  const productElements = products.map(item => (
+    <div key={item._id}>
+      <Grid.Column>
+        <ProductCart product={item} />
+      </Grid.Column>
+    </div>
+  ));
+  if (!loading) return <div>loading</div>;
   return (
     <div className="container">
-      <Card.Group itemsPerRow={4}>
-        {products}
-      </Card.Group>
+      <Segment>
+        <Header>{`CATEGORIES ${id}`}</Header>
+        <Grid container columns={3} centered>
+          {productElements}
+        </Grid>
+      </Segment>
     </div>
-  	)
+  );
 };
+
+const mapStateToProps = state => ({
+  products: state.productsByFilter.products.products,
+  loading: state.productsByFilter.loading
+});
+
+const mapDispatchToProps = dispatch => ({
+  onGetProductsByFilter: query => {
+    dispatch(getProductsByFilterQuery(query));
+  }
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductDetails);
