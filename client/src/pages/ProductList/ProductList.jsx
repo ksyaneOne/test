a629d3ecab5726ef5f5e36959b364bf1b3246ce4
Sopruk <alexandr.programer@gmail.com>
2020-01-04 20:35:02 +0100

    added possible to fetch more products

--- client/src/pages/ProductList/ProductList.jsx
+++ client/src/pages/ProductList/ProductList.jsx
@@ -1,48 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Dimmer, Loader } from 'semantic-ui-react';
-import { getProductsByFilterQuery, getProducts } from '../../actions/products';
-import { Segment, Header, Grid } from 'semantic-ui-react';
+import { getProductsByFilterQuery, getProducts, getMoreProducts } from '../../actions/products';
+import { Segment, Header, Grid, Button } from 'semantic-ui-react';
+import { BtnWrapper } from './style';
 import ProductCart from '../../components/CarouselNewProducts/ProductCard';
 
 const ProductDetails = props => {
   const {
     onGetProductsByFilter,
-    onGetAllProducts,
+    onGetMoreProducts,
     match,
-    allProducts,
     productsByQuery,
+    moreProducts,
     loadingByFilter,
-    loadingAllProducts
+    loadingMoreProducts
   } = props;
 
-  const { params, path, url } = match;
+  const { params, path } = match;
   const { id } = params;
   const initialState = [];
+  const [prevProducts, setPrevProducts] = useState(initialState);
   const [products, setProducts] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [typeOfProducts, setTypeOfProducts] = useState('');
+  const [scrollTop, setScrollTop] = useState(0);
+  const [lastItem, setLastItem] = useState('');
+  const [isShownBtn, setIsShownBtn] = useState(false);
+  const [quantityProducts, setQuantityProducts] = useState(3);
 
   useEffect(() => {
     switch (path) {
       case '/products':
         setTypeOfProducts('All products');
-        onGetAllProducts();
+        setPrevProducts([]);
+        setProducts([]);
+        onGetMoreProducts(quantityProducts, '');
         break;
       case '/categories/:id':
         setTypeOfProducts(`CATEGORIES ${id}`);
+        setIsShownBtn(false);
         onGetProductsByFilter(`categories=${id}`);
         break;
       default:
         setProducts([]);
     }
-  }, [url, onGetAllProducts, onGetProductsByFilter, id, path]);
+  }, [id, path]);
 
   useEffect(() => {
-    if (allProducts && path === '/products') {
-      setProducts(allProducts);
+    setPrevProducts(products);
+    onGetMoreProducts(quantityProducts, lastItem);
+  }, [lastItem]);
+
+  useEffect(() => {
+    if (moreProducts && path === '/products') {
+      setProducts([].concat(prevProducts, moreProducts));
     }
-  }, [allProducts, path]);
+  }, [moreProducts]);
+
+  useEffect(() => {
+    if (path === '/products') {
+      if (moreProducts.length === 0 && products.length !== quantityProducts) {
+        setIsShownBtn(false);
+      } else {
+        setIsShownBtn(true);
+      }
+      window.scrollTo({
+        top: scrollTop,
+        behavior: 'smooth'
+      });
+    }
+  }, [products]);
 
   useEffect(() => {
     if (productsByQuery !== undefined && path === '/categories/:id') {
@@ -55,8 +83,18 @@ const ProductDetails = props => {
   }, [loadingByFilter]);
 
   useEffect(() => {
-    setLoading(loadingAllProducts);
-  }, [loadingAllProducts]);
+    setLoading(loadingMoreProducts);
+  }, [loadingMoreProducts]);
+
+  const loadMoreClick = () => {
+    if (moreProducts.length > 0) {
+      let curentScroll = window.pageYOffset || document.documentElement.scrollTop;
+      let id = moreProducts.slice(-1)[0]._id;
+
+      setScrollTop(curentScroll);
+      setLastItem(id);
+    }
+  };
 
   const productElements = products.map(item => (
     <div key={item._id}>
@@ -68,7 +106,7 @@ const ProductDetails = props => {
   if (!loading)
     return (
       <Dimmer active>
-        <Loader />
+        <Loader indeterminate>loading...</Loader>
       </Dimmer>
     );
   return (
@@ -81,6 +119,15 @@ const ProductDetails = props => {
           <Grid container columns={3} centered>
             {productElements}
           </Grid>
+          <Grid centered>
+            {isShownBtn ? (
+              <BtnWrapper>
+                <Button onClick={loadMoreClick} content="Load more..." secondary />
+              </BtnWrapper>
+            ) : (
+              ''
+            )}
+          </Grid>
         </Segment>
       </Segment>
     </div>
@@ -90,8 +137,10 @@ const ProductDetails = props => {
 const mapStateToProps = state => ({
   productsByQuery: state.productsByFilter.products.products,
   allProducts: state.products.products,
+  moreProducts: state.productsMore.products,
   loadingByFilter: state.productsByFilter.loading,
-  loadingAllProducts: state.products.loading
+  loadingAllProducts: state.products.loading,
+  loadingMoreProducts: state.productsMore.loading
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -100,6 +149,9 @@ const mapDispatchToProps = dispatch => ({
   },
   onGetAllProducts: () => {
     dispatch(getProducts());
+  },
+  onGetMoreProducts: (limit, lastProduct) => {
+    dispatch(getMoreProducts(limit, lastProduct));
   }
 });
 export default connect(mapStateToProps, mapDispatchToProps)(ProductDetails);
